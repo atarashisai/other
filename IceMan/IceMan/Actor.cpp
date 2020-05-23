460d05205c27ac1c571326ed9cdf26e0c727e26d
atarashisai <42406113+atarashisai@users.noreply.github.com>
2020-05-22 23:04:25 -0700

    Day1 Progress

--- IceMan/IceMan/Actor.cpp
+++ IceMan/IceMan/Actor.cpp
@@ -1,4 +1,173 @@
 #include "Actor.h"
 #include "StudentWorld.h"
-
+#include <assert.h>
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
+void Gold::doSomething() {
+	if (!_alive) return;
+	if (isPermanent) {
+		StudentWorld* world = static_cast<StudentWorld*>(this->_gw);
+		Iceman* player = static_cast<Iceman*>(world->getPlayer());
+		if (std::abs((this->x() + 1 - player->x())) < 4 && std::abs((this->y() + 1 - player->y())) < 4) {
+			setVisible(true);
+		}
+		if (std::abs((this->x() + 1 - player->x())) < 3 && std::abs((this->y() + 1 - player->y())) < 3) {
+			player->getGold();
+			world->increaseScore(10);
+			kill();
+		}
+	}
+	else {
+		this->countdown -= 1;
+		if (this->countdown <= 0) {
+			setVisible(false);
+			this->_alive = false;
+		}
+	}
+
+}
+void Barrel::doSomething() {
+	if (!_alive) return;
+	StudentWorld* world = static_cast<StudentWorld*>(this->_gw);
+	Iceman* player = static_cast<Iceman*>(world->getPlayer());
+	if (std::abs((this->x() + 1 - player->x())) < 4 && std::abs((this->y() + 1 - player->y())) < 4) {
+		setVisible(true);
+	}
+	if (std::abs((this->x() + 1 - player->x())) < 3 && std::abs((this->y() + 1 - player->y())) < 3) {
+		world->increaseScore(1000);
+		kill();
+	}
+}
+void Boulder::doSomething() {
+
+}
+void Boulder::removeIce() {
+	for (int deltaX = 0; deltaX < SPRITE_WIDTH; deltaX++) {
+		for (int deltaY = 0; deltaY < SPRITE_HEIGHT; deltaY++) {
+			Actor* tile = static_cast<StudentWorld*>(this->_gw)
+				->getTile(this->_x + deltaX, this->_y + deltaY);
+			if (tile != nullptr) {
+				tile->kill();
+			}
+		}
+	}
+}
+
+
+void Sonar::doSomething() {
+	if (!_alive) return;
+	this->countdown -= 1;
+	if (this->countdown <= 0) {
+		setVisible(false);
+		this->_alive = false;
+	}
+	StudentWorld* world = static_cast<StudentWorld*>(this->_gw);
+	Iceman* player = static_cast<Iceman*>(world->getPlayer());
+	if (std::abs((this->x() + 1 - player->x())) < 3 && std::abs((this->y() + 1 - player->y())) < 3) {
+		player->getSonar();
+		world->increaseScore(75);
+		kill();
+	}
+}
+
+void Water::doSomething() {
+	if (!_alive) return;
+	this->countdown -= 1;
+	if (this->countdown <= 0) {
+		setVisible(false);
+		this->_alive = false;
+	}
+	StudentWorld* world = static_cast<StudentWorld*>(this->_gw);
+	Iceman* player = static_cast<Iceman*>(world->getPlayer());
+	if (std::abs((this->x() + 1 - player->x())) < 3 && std::abs((this->y() + 1 - player->y())) < 3) {
+		player->getSquirt();
+		world->increaseScore(100);
+		kill();
+	}
+}
+
+void Iceman::doSomething() {
+	int ch;
+	if (getWorld()->getKey(ch) == true)
+	{
+		// user hit a key this tick!
+		switch (ch)
+		{
+		case KEY_PRESS_LEFT:
+			/*... move player to the left ...;*/
+			setDirection(left);
+			if (this->_x > 0) {
+				this->_x -= 1;
+				removeTile();
+			}
+			moveTo(this->_x, this->_y);
+			break;
+		case KEY_PRESS_RIGHT:
+			/*... move player to the right ...; */
+			setDirection(right);
+			if (this->_x < VIEW_WIDTH - SPRITE_WIDTH) {
+				this->_x += 1;
+				removeTile();
+			}
+			moveTo(this->_x, this->_y);
+			break;
+		case KEY_PRESS_DOWN:
+			setDirection(down);
+			if (this->_y > 0) {
+				this->_y -= 1;
+				removeTile();
+			}
+			moveTo(this->_x, this->_y);
+			break;
+		case KEY_PRESS_UP:
+			setDirection(up);
+			if (this->_y < VIEW_HEIGHT - SPRITE_HEIGHT) {
+				this->_y += 1;
+				removeTile();
+			}
+			moveTo(this->_x, this->_y);
+			break;
+		case KEY_PRESS_SPACE:
+			/*... add a Squirt in front of the player...; */
+			break;
+			// etc…
+		}
+	}
+}
+int Iceman::gold() {
+	return this->number_gold;
+}
+int Iceman::sonar() {
+	return this->number_sonar;
+}
+int Iceman::squirt() {
+	return this->number_squirt;
+}
+int Iceman::barrel() {
+	return this->number_barrel;
+}
+int Iceman::health() {
+	return this->hit_point;
+}
+void Iceman::getGold() {
+	number_gold += 1;
+}
+void Iceman::getSonar() {
+	number_sonar += 1;
+}
+void Iceman::getSquirt() {
+	number_squirt += 5;
+}
+void Iceman::getBarrel() {
+	number_barrel += 1;
+}
+void Iceman::removeTile() const {
+	for (int deltaX = 0; deltaX < SPRITE_WIDTH; deltaX++) {
+		for (int deltaY = 0; deltaY < SPRITE_HEIGHT; deltaY++) {
+			Actor* tile = static_cast<StudentWorld*>(this->_gw)
+				->getTile(this->_x + deltaX, this->_y + deltaY);
+			if (tile != nullptr) {
+				tile->kill();
+			}
+		}
+	}
+}
--- IceMan/IceMan/Actor.h
+++ IceMan/IceMan/Actor.h
@@ -2,7 +2,144 @@
 #define ACTOR_H_
 
 #include "GraphObject.h"
+#include "GameWorld.h"
+#include <algorithm>
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class Actor : public GraphObject {
+private:
+	/* */
+protected:
+	GameWorld* _gw;
+	int _x;
+	int _y;
+	bool _alive;
+public:
+	Actor(GameWorld* gw, int imageID, bool visible, int startX, int startY, Direction dir = right, double size = 1.0, unsigned int depth = 0)
+		: GraphObject(imageID, startX, startY, dir, size, depth) {
+		this->_x = startX;
+		this->_y = startY;
+		this->_gw = gw;
+		this->_alive = true;
+		setVisible(visible);
+	}
+	virtual ~Actor() { }
+	virtual void doSomething() {}
+	virtual GameWorld* getWorld() { return this->_gw; };
+	virtual void kill() {
+		this->_alive = false;
+		this->setVisible(false);
+	}
+	int x() { return _x; };
+	int y() { return _y; };
+};
+class Ice : public Actor {
+public:
+	Ice(GameWorld* gw, int x, int y): 
+		Actor(gw, IID_ICE, true, x, y, right, 0.25, 3) {};
+	void doSomething() {};
+	~Ice() { }
+};
+class Boulder : public Actor {
+public:
+	Boulder(GameWorld* gw, int x, int y): 
+		Actor(gw, IID_BOULDER, true, x, y, down, 1, 1) {
+		removeIce();
+	};
+	virtual void doSomething();
+	void removeIce();
+	~Boulder() { }
+};
+class Goody : public Actor {
+public:
+	using Actor::Actor;
+	void doSomething() {}
+	virtual ~Goody() {}
+};
+class Sonar : public Goody {
+private:
+	int countdown;
+public:
+	Sonar(GameWorld* gw, int x, int y) :
+		Goody(gw, IID_SONAR, true, x, y, right, 1, 2), 
+		countdown(std::max(100, 300 - 10 * (int)gw->getLevel())) {}
+	void doSomething();
+	~Sonar() {}
+};
 
+class Water : public Goody {
+private:
+	int countdown;
+public:
+	Water(GameWorld* gw, int x, int y) :
+		Goody(gw, IID_WATER_POOL, true, x, y, right, 1, 2) {}
+	void doSomething();
+	~Water() {}
+};
+
+class Gold : public Goody {
+private:
+	int countdown;
+	bool isPermanent = true;
+public:
+	Gold(GameWorld* gw, int x, int y) :
+		Goody(gw, IID_GOLD, false, x, y, right, 1, 2) {}
+	void doSomething();
+	~Gold() {}
+};
+
+class Barrel : public Goody {
+private:
+	int countdown;
+public:
+	Barrel(GameWorld* gw, int x, int y):
+		Goody(gw, IID_BARREL, false, x, y, right, 1, 2) {}
+	void doSomething();
+	~Barrel() {}
+};
+class Iceman : public Actor {
+private:
+	int number_barrel;
+	int number_squirt;
+	int number_sonar;
+	int number_gold;
+	int hit_point;
+public:
+	Iceman(GameWorld* gw, int x = 30, int y = 60) :
+		Actor(gw, IID_PLAYER, true, x, y, right, 1, 0),
+		number_barrel(0),
+		number_squirt(5),
+		number_sonar(1), 
+		number_gold(0),
+		hit_point(10) {}
+	virtual void doSomething();
+	~Iceman() {}
+	int gold();
+	int sonar();
+	int squirt();
+	int health();
+	int barrel();
+	void getGold();
+	void getSonar();
+	void getSquirt();
+	void getBarrel();
+	void removeTile() const;
+};
+
+class Protester : public Actor {
+private:
+	/* */
+public:
+	/* */
+	Protester();
+	~Protester();
+};
+class HardcoreProtester : public Protester {
+private:
+	/* */
+public:
+	/* */
+	HardcoreProtester();
+	~HardcoreProtester();
+};
 #endif // ACTOR_H_
